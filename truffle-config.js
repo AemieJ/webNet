07c4bf4b0c795293d43b23c4113eecf136ad4c3e
AemieJ <aemie.j@hotmail.co.uk>
2020-01-06 20:53:52 +0530

    deploy contract to ropsten test network

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 require('babel-register');
 require('babel-polyfill');
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
   networks: {
@@ -8,6 +11,16 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC, 
+          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`
+        )
+      },
+      network_id: 3,
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
